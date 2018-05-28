c215c6a141407f74b75d24c3a98e30137f8f1b1e
Mth--bs-park <bbeomssoo@gmail.com>
2018-05-28 17:26:15 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// import $ from 'jquery';
+
 class App extends Component {
 
   constructor() {
@@ -12,15 +14,89 @@ class App extends Component {
       isMobile,
       browserHeight: isMobile ? browserHeight : 0
     };
-    console.log(this.state);
   }
 
   render() {
+
+    // const items = [
+    //   {},
+    //   {},
+    //   {},
+    //   {}
+    // ];
+
     return (
       <div>
         <Navigation />
-        <div className="container">
-          <h1>Test</h1>
+        <div className="AppWrap">
+          <div className="row page-one">
+            <div className="m-auto col-md-10">
+              <h1 className="m-t-89 text-center text-light">
+                Level up your real estate investment
+              </h1>
+              <p className="m-t-45 text-center text-light">
+                Meridio enables you to create and invest in shares of individual properties on blockchain
+              </p>
+              <div className="text-center btn-wrap">
+                <button className="btn btn-ubscribe">Subscribe</button>
+              </div>
+            </div>
+            <div className="shape row">
+              <div className="shapeL col-md-6"></div>
+              <div className="shapeR col-md-6"></div>
+            </div>
+          </div>
+          <div className="row page-two">
+            <div className="m-auto col-md-10">
+              <span className="title">For Investor:</span>
+              <ul className="list">
+                <li>Tell your friends</li>
+                <li>Tell your friends</li>
+                <li>Tell your friends</li>
+                <li>Tell your friends</li>
+              </ul>
+              <div className="btn-wrap">
+                <button className="btn btn-ubscribe">Subscribe</button>
+              </div>
+            </div>
+            <div className="shape row">
+              <div className="shapeC col-md-12"></div>
+            </div>
+          </div>
+          <div className="row page-three">
+            <div className="m-auto col-md-10">
+              <span className="title">For Management Compompanies:</span>
+              <ul className="list">
+                <li>Tell your friends</li>
+                <li>Tell your friends</li>
+                <li>Tell your friends</li>
+                <li>Tell your friends</li>
+              </ul>
+              <div className="btn-wrap">
+                <button className="btn btn-ubscribe">Subscribe</button>
+              </div>
+            </div>
+            <div className="shape row">
+            </div>
+          </div>
+        </div>
+        <div className="row page-four">
+          <div className="m-auto col-md-10">
+            <h1 className="m-t-89 text-center text-purple">
+              Level up your real estate investment
+            </h1>
+            <p className="m-t-45 text-center text-purple">
+              Meridio enables you to create and invest in shares of individual properties on blockchain
+            </p>
+            <div className="text-center btn-wrap">
+              <div className="input-group">
+                <input type="text" className="inputMail form-control" placeholder=""/>
+                <button className="btn btn-ubscribe">Subscribe</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row footer">
         </div>
       </div>
 
@@ -47,51 +123,44 @@ class Navigation extends Component {
 
   constructor() {
     super();
-    this.toggle = this.toggle.bind(this);
   }
 
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-md navbar-dark bg-purple fixed-top">
         <a className="navbar-brand" href="/">Terrario</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" onClick={this.toggle}>
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div className="navbar-collapse collapse show" id="navbarSupportedContent">
+        <div className="navbar-collapse collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ml-auto navbar-right text-center">
-            <li className="nav-item active">
-              <a className="nav-link" href="/blog">Blog</a>
+            <li className="nav-item">
+              <a className="nav-link" href="#blog">Blog</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/contract">Contract</a>
+              <a className="nav-link" href="#contract">Contract</a>
             </li>
           </ul>
         </div>
       </nav>
     );
   }
-
-  toggle(event) {
-    const e = event.nativeEvent;
-    console.log(e);
-
-  }
 }
-
-class Section extends Component {
-
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return(
-      <div style={{height: `${this.props.height}px`}}>
-      </div>
-    );
-  }
-}
+//
+// class Section extends Component {
+//
+//   constructor(props) {
+//     super(props);
+//   }
+//
+//   render() {
+//     return(
+//       <div style={{height: `${this.props.height}px`}}>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
